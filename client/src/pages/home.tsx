0544639b2629068f4b09f80e23182d3f0436cbe5
abeel50 <abeelmuhammad50@gmail.com>
2023-03-08 11:17:37 +0500

    Pie Charts

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useList } from '@pankod/refine-core';
-
+import { Box, Typography, Stack } from '@pankod/refine-mui';
 import {
     PieChart,
     PropertyReferrals,
@@ -12,7 +12,27 @@ import {
 
 const home = () => {
     return (
-        <div>home</div>
+        <Box>
+            <Typography fontSize={25} fontWeight={700} color={"#11142D"}>
+                Dashboard
+            </Typography>
+
+            <Box mt={"20px"} display={"flex"} flexWrap={"wrap"} gap={4}>
+                <PieChart title="Properties for Sale" value={684}
+                    series={[75, 25]} colors={["#275be8", "#c4e8ef"]} />
+
+                <PieChart title="Properties for Rent" value={550}
+                    series={[60, 40]} colors={["#275be8", "#c4e8ef"]} />
+
+                <PieChart title="Total Customers" value={5000}
+                    series={[75, 25]} colors={["#275be8", "#c4e8ef"]} />
+
+                <PieChart title="Total Cities" value={55}
+                    series={[70, 35]} colors={["#275be8", "#c4e8ef"]} />
+
+            </Box>
+
+        </Box>
     )
 }
 
