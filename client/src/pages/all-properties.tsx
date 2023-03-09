07a78de92192b7fbef0602fc2c642f9d0a58631b
abeel50 <abeelmuhammad50@gmail.com>
2023-03-09 19:46:26 +0500

    Property Listing

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Add } from '@mui/icons-material';
-import { useList } from '@pankod/refine-core/dist/hooks';
+import { useTable } from '@pankod/refine-core';
 import { Box, Typography, Stack } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 import { PropertyCard, CustomButton } from 'components';
@@ -8,6 +8,11 @@ import { PropertyCard, CustomButton } from 'components';
 
 const AllProperties = () => {
     const navigate = useNavigate();
+    const { tableQueryResult: { data, isLoading, isError } } = useTable();
+    const allProperties = data?.data ?? [];
+
+    if (isLoading) return <Typography>Loading...</Typography>
+    if (isError) return <Typography>Error...</Typography>
 
     return (
         <Box>
@@ -19,8 +24,15 @@ const AllProperties = () => {
                     handleClick={() => navigate('/properties/create')}
                     backgroundColor="#475be8" color="#fcfcfc"
                     icon={<Add />} />
-
             </Stack>
+            <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }}>
+                {allProperties.map((property) => (
+                    <PropertyCard key={property._id} id={property._id} title={property.title}
+                        price={property.price} location={property.location} photo={property.photo} />
+
+                ))}
+
+            </Box>
         </Box>
     )
 }
